4624100995a556d94f4cd35fbb2dee1a6079f022
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-01-07 18:45:34 +0800

    app: update startDestination on MainActivity

--- app/src/main/java/com/ralphmarondev/lumi/MainActivity.kt
+++ app/src/main/java/com/ralphmarondev/lumi/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.Crossfade
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.ralphmarondev.core.data.local.preferences.AppPreferences
+import com.ralphmarondev.core.presentation.component.LumiSplash
 import com.ralphmarondev.core.presentation.theme.LocalThemeState
 import com.ralphmarondev.core.presentation.theme.LumiTheme
 import com.ralphmarondev.core.presentation.theme.ThemeProvider
 import com.ralphmarondev.lumi.navigation.AppNavigation
+import com.ralphmarondev.lumi.navigation.Routes
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +34,31 @@ class MainActivity : ComponentActivity() {
                 LumiTheme(
                     darkTheme = themeState.darkTheme.value
                 ) {
-                    AppNavigation()
+                    var startDestination by remember { mutableStateOf<Routes?>(null) }
+
+                    LaunchedEffect(Unit) {
+                        delay(2000)
+                        val setupCompleted = preferences.isSystemOnboardingCompleted().first()
+                        val authenticated = preferences.isAuthenticated().first()
+                        val enabledAuth = preferences.isSystemEnableAuth().first()
+
+                        startDestination = when {
+                            setupCompleted && authenticated && !enabledAuth -> Routes.Launcher
+                            setupCompleted && authenticated && enabledAuth -> Routes.Login
+                            setupCompleted -> Routes.Login
+                            else -> Routes.Setup
+                        }
+                    }
+
+                    Crossfade(
+                        targetState = startDestination,
+                        label = "splashToApp"
+                    ) { destination ->
+                        when (destination) {
+                            null -> LumiSplash()
+                            else -> AppNavigation(startDestination = destination)
+                        }
+                    }
                 }
             }
         }
