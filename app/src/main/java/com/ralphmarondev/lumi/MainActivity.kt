6f9d771cf25ef72e36ed99ee742a778d61670692
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-01-13 16:00:58 +0800

    app: fix issue on status bars
    - Our custom status bar is overlapping the topappbars,
      so we fixed it by putting back enableEdgeToEdge()
      on our MainActivity.

--- app/src/main/java/com/ralphmarondev/lumi/MainActivity.kt
+++ app/src/main/java/com/ralphmarondev/lumi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ralphmarondev.lumi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,6 +28,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
         @Suppress("DEPRECATION")
