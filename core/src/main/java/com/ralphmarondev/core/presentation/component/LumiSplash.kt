7984dd0bd9c6c2564888193c812a12cb2217fb61
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-01-17 16:44:12 +0800

    core: update splash

--- core/src/main/java/com/ralphmarondev/core/presentation/component/LumiSplash.kt
+++ core/src/main/java/com/ralphmarondev/core/presentation/component/LumiSplash.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.ralphmarondev.core.R
@@ -33,7 +35,8 @@ import com.ralphmarondev.core.R
 fun LumiSplash() {
     Box(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -78,6 +81,15 @@ fun LumiSplash() {
             Spacer(modifier = Modifier.height(32.dp))
             DotsLoader()
         }
+        Text(
+            text = "Developed by\nRalph Maron Eda",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 24.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
