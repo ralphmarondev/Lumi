6d99c4f09cc289c3b3a3c0ddd63efc0b6145f8fb
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-02-14 16:50:19 +0800

    core: rename LumiApp.kt

--- core/src/main/java/com/ralphmarondev/core/domain/model/LumiApp.kt
+++ core/src/main/java/com/ralphmarondev/core/domain/model/LumiApp.kt
@@ -1,11 +1,11 @@
 package com.ralphmarondev.core.domain.model
 
-data class Apps(
+data class LumiApp(
     val id: Long = 0,
     val name: String = "",
     val icon: String = "",
     val versionName: String = "",
-    val versionCode: Int = 0,
+    val versionCode: Int = 1,
     val isSystemApp: Boolean = false,
     val isDocked: Boolean = false,
     val isInstalled: Boolean = true,
