d84be4af494f765097c09c5744594288440cca8b
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-02-20 10:03:28 +0800

    gradle: add firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.ksp) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
